247d1f08332492b6741ec4dbd5dc7eb8fe704e25
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-10-21 17:18:35 +0600

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector(".menu_bar");
 const nav = document.getElementById("nav_menu");
+const clients = document.querySelectorAll('.client');
+const list = document.querySelector('.client-list');
 
 menu.addEventListener("click", () => {
     let ami = nav_menu.classList.toggle("active");
@@ -9,3 +11,24 @@ menu.addEventListener("click", () => {
         menu.innerHTML ='&#9776' 
     }
 });
+list.addEventListener('scroll', () => {
+    const clientHeight = clients[0].offsetHeight; 
+    const scrollTop = list.scrollTop; 
+    const totalClients = clients.length;
+
+    const index = Math.floor(scrollTop / clientHeight);
+    clients.forEach((client) => {
+        client.classList.remove('active');
+        client.style.opacity = '0.5';
+        client.style.color = 'gray';
+        client.style.transform = 'rotateY(0deg)';
+    });
+
+    if (index < totalClients) {
+        clients[index].classList.add('active');
+        clients[index].style.opacity = '1';
+        clients[index].style.color = '#000';
+        clients[index].style.transform = 'rotateY(360deg)';
+    }
+});
+
